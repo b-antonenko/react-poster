6212fde46b38716443d476ba45b3698d3846f3bd
AB <b_antonenko@icloud.com>
2025-03-19 19:56:35 +0200

    Initial

---
+++ src/components/Post.jsx
@@ -0,0 +1,14 @@
+const Post = () => {
+    return (
+        <div>
+            <p>
+                Bohdan
+            </p>
+            <p>
+                React.js is great!
+            </p>
+        </div>
+    );
+};
+
+export default Post;
